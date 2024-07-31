8cb92b4886026250f501b25106dd6811fc220052
sedlukha <sedlukha@icloud.com>
2024-07-31 09:28:51 +0300

    Initial commit from Create Next App

---
+++ home/next.config.js
@@ -0,0 +1,19 @@
+const { BLOG_URL } = process.env;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/blog",
+        destination: `${BLOG_URL}/blog`,
+      },
+      {
+        source: "/blog/:path+",
+        destination: `${BLOG_URL}/blog/:path+`,
+      },
+    ];
+  },
+};
+
+module.exports = nextConfig;
