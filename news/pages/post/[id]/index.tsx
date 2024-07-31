b669fdfea8865237bc5202771141fbfc72568dd3
sedlukha <sedlukha@icloud.com>
2024-07-31 09:55:08 +0300

    fix news params error

--- news/pages/post/[id]/index.tsx
+++ news/pages/post/[id]/index.tsx
@@ -1,9 +1,9 @@
 import Link from "next/link";
 
-export default function Post({ params: { id } }) {
+export default function Post() {
   return (
     <div>
-      <h3>Post #{id}</h3>
+      <h3>Post</h3>
       <p>Lorem ipsum</p>
       <Link href="/news">Back to blog</Link>
     </div>
